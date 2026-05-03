78bdc93f1da7e4edd24e5819a1225e14c0314334
kevinbao1012 <kevthebaozi@gmail.com>
2026-05-03 11:11:22 -0400

    Update queries.sql join everything together

--- 01-library-management/queries.sql
+++ 01-library-management/queries.sql
@@ -1,1 +1,4 @@
-
+SELECT members.first_name, members.last_name, books.title, checkouts.checkout_date
+FROM checkouts
+JOIN members ON checkouts.member_id = members.member_id
+JOIN books ON checkouts.book_id = books.book_id;
